efd1efb6e11415e5fabd43f434b39adb5aba8712
Grizzmon <152689097+Grizzmon@users.noreply.github.com>
2026-08-13 13:16:11 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://cjxfvpkbfixjkppowhwg.supabase.co'
-const supabaseAnonKey = 'sb_publishable_0IuI2B5FJ6DmPREfRCqjqG__stdi'
+const supabaseUrl = "https://cjxfvpkbfixjkppowhwg.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqeGZ2cGtiZml4amtwcG93aHdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyOTg3NzMsImV4cCI6MjA5Mzg3NDc3M30.8Z9WJ_HPY2MS_LKFol2bZ2MAYzlqCpR9E0oV4oOV5Ew";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
